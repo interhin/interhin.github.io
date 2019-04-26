b62afc1edaa93752a0400097745289c5dbdb2289
Dmitriy Vergulev <diman7277@gmail.com>
2019-04-26 22:37:45 +0700

    Added tooltip and modal window for lingua

--- lingua/js/index.js
+++ lingua/js/index.js
@@ -27,10 +27,22 @@ $(document).ready(function(){
                 slidesToShow: 1,
                 slidesToScroll: 1
               }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                arrows:false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+             }
             }
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
           ]
       });
+
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
 });
