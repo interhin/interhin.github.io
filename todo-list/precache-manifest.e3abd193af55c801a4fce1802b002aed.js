9fc254ed703ed8628c7410009bace13bb2023683
Dmitriy Vergulev <diman7277@gmail.com>
2019-06-11 19:26:59 +0700

    added todo-list

---
+++ todo-list/precache-manifest.e3abd193af55c801a4fce1802b002aed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eff7593c819a262115bab02abb4a97c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "707d6b4773af09699126",
+    "url": "/static/css/main.28a56cb3.chunk.css"
+  },
+  {
+    "revision": "53b0cbec0584efb29fa3",
+    "url": "/static/js/2.ef5cab02.chunk.js"
+  },
+  {
+    "revision": "707d6b4773af09699126",
+    "url": "/static/js/main.ac40e860.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
